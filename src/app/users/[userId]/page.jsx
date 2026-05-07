aa497550c763176d12fe6e3ae1195a4d294d3898
emon-101 <emon26.ugv@gmail.com>
2026-05-07 12:46:24 +0600

    fetch dynamic data from the users and display the data in user details page

---
+++ src/app/users/[userId]/page.jsx
@@ -0,0 +1,15 @@
+import { getUserById } from '@/app/lib/data';
+import React from 'react';
+
+const UserDetailsPage = async({params}) => {
+    const {userId} = await params;
+    const user = await getUserById(userId);
+    console.log(user);
+    return (
+        <div>
+            <h2>User Details : {user.name}</h2>
+        </div>
+    );
+};
+
+export default UserDetailsPage;
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -4,7 +4,6 @@ import { getUsers } from "../lib/data";
 
 const UsersPage = async() => {
     const users = await getUsers();
-    console.log(users);
     return (
         <div>
             <h1>User Management : {users.length}</h1>
